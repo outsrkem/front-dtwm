cdca325d708faf40c1b3449179d35432c0baa3da
Lark <lark.email@lark.com>
2026-07-16 16:34:46 +0800

    feat: support rpm package building

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import router from "./router";
 import EventBusPlugin from "./utils/event-bus.js";
-import "element-plus/dist/index.css";
+import vueCookies from "vue-cookies";
 import "./styles/normalize.css";
 import "./styles/index.less";
 import App from "./App.vue";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import zhCn from "element-plus/es/locale/lang/zh-cn";
-import config from "./config/config";
 
-import vueCookies from "vue-cookies";
 const app = createApp(App);
 
-// 注册所有 ElementPlus 图标
+// Register all element-plus icon components globally
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
 
-app.config.globalProperties.$config = config;
+// Register global plugins in order
 app.use(ElementPlus, {
     locale: zhCn,
 });
@@ -26,9 +25,10 @@ app.use(router);
 app.use(EventBusPlugin);
 app.use(vueCookies);
 
-// 全局修改 ElDialog 默认配置（点击空白/ESC 不关闭弹窗）
-app._context.components.ElDialog["props"].closeOnClickModal.default = false;
-app._context.components.ElDialog["props"].closeOnPressEscape.default = false;
+// Override default ElDialog props
+// Disable close dialog by clicking mask or pressing ESC key
+app._context.components.ElDialog.props.closeOnClickModal.default = false;
+app._context.components.ElDialog.props.closeOnPressEscape.default = false;
 
-// 挂载app
+// Mount root app to #app dom node
 app.mount("#app");
